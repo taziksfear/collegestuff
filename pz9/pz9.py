092e3fd4adb7a22354c3978533777598703bb149
tazuwuik aka tatik <tazik6104@gmail.com>
2025-06-09 01:37:22 +0000

    pz9

---
+++ pz9/pz9.py
@@ -0,0 +1,13 @@
+words = {
+    'apple': 'яблоко',
+    'house': 'дом',
+    'cat': 'кот',
+    'dog': 'собака',
+    'book': 'книга',
+    'water': 'вода'
+}
+
+rus_words = {v: k for k, v in words.items()}
+
+word = input('Введите слово: ').lower()
+print(words.get(word, rus_words.get(word, 'Нет в словаре')))
